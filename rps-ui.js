c4822a01e135c182ed7d07a8df82cdbc5c205e16
shiela <sheshemarie31@gmail.com>
2022-05-09 11:41:00 +0800

    change functions and put queries in their repspective functions to be more readable and understandable

--- rps-ui.js
+++ rps-ui.js
@@ -1,106 +1,112 @@
-    //scoreboard
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    //queries
-    const scoreBoard = document.querySelector('.score-board');
-    const playerHand = document.querySelector('.player-hand');
-    const computerHand = document.querySelector('.computer-hand');
-    const scoreLeft = document.querySelector('.player-current');
-    const scoreRight = document.querySelector('.computer-current');
-    const gameResult = document.querySelector('.game-result');
-    
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-      button.addEventListener('click', () => {
-        let playerSelection = button.className;
-        let computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection, computerSelection);
-    
-        if(roundResult === 'playerWin') {
-          playerScore++
-        } else if (roundResult === 'computerWin') {
-          computerScore++;
-        }
-        
-    
-        //Player Pick Illustrations
-        function changeImage() {
-        if(playerSelection === 'rock') {
-          playerHand.setAttribute('src', './images/human_rock.png');
-        } else if (playerSelection === 'paper') {
-          playerHand.setAttribute('src', './images/human_paper.png');
-        } else if (playerSelection === 'scissors') {
-          playerHand.setAttribute('src', './images/human_scissors.png');
-        }
-    
-        //Computer Pick Illustrations
-        if(computerSelection === 'rock') {
-          computerHand.setAttribute('src', './images/robot_rock.png');
-        } else if(computerSelection === 'paper') {
-          computerHand.setAttribute('src', './images/robot_paper.png');
-        } else if(computerSelection === 'scissors') {
-          computerHand.setAttribute('src', './images/robot_scissors.png');
-        }
-      }
-      score();
+function game() {
+  //scoreboard
+let playerScore = 0;
+let computerScore = 0;
+
+//Query to change game result text;
+const gameResult = document.querySelector('.game-result');
+
+//To start the game
+const startGame = () => {
+  const startBtn = document.querySelector('.first-page button');
+  const firstPage = document.querySelector('.first-page');
+  const game = document.querySelector('.game');
+
+  startBtn.addEventListener('click', () => {
+    firstPage.classList.add('fadeOut');
+    game.classList.add('fadeIn');
+  });
+}
+
+const playMatch = () => {
+  const buttons = document.querySelectorAll('.buttons button');
+  const playerHand = document.querySelector('.player-hand');
+  const computerHand = document.querySelector('.computer-hand');
+
+  //to put arguments in playRound function
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const playerSelection = button.className;
+      const computerSelection = computerPlay();
+      let roundResult = playRound(playerSelection, computerSelection);
+
+      //change images
+      playerHand.src = `./images/human_${playerSelection}.png`;
+      computerHand.src = `./images/robot_${computerSelection}.png`;
+
+      //to end game
       gameEnd();
-      changeImage();
-      })
     })
-    
-    function computerPlay() {
-      const pick = ['rock', 'paper', 'scissors'];
-      return pick[Math.floor(Math.random() * pick.length)];
+  })
+}
+
+const gameEnd = () => {
+  //create restart button
+  const restartBtn = document.createElement('button');
+  restartBtn.className = 'restart';
+  restartBtn.textContent = 'Play Again'
+
+  if(playerScore === 5 || computerScore === 5) {
+   document.querySelector('.rock').style.display = 'none';
+   document.querySelector('.paper').style.display = 'none';
+   document.querySelector('.scissors').style.display = 'none';
+   document.querySelector('.buttons').appendChild(restartBtn);
+
+   //to restart
+   restartBtn.addEventListener('click', () => {
+     window.location.reload();
+   })
+   
+    if(playerScore > computerScore) {
+      gameResult.textContent = 'Yey! You win!';
+    } else if (computerScore > playerScore) {
+      gameResult.textContent = 'Aww you lose against the computer';
     }
-    
-    function playRound(playerSelection, computerSelection) {
-    
-      let tie = 'Draw!';
-      let playerWin = 'You win this round!';
-      let computerWin = 'You lose this round!';
-    
-      if(playerSelection === computerSelection) {
-        gameResult.innerHTML = tie;
-        return 'tie';
-      } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-        gameResult.innerHTML = playerWin;
-        return 'playerWin';
-      } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        gameResult.innerHTML = playerWin;
-        return 'playerWin';
-      } else if(playerSelection === 'scissors' && computerSelection === 'paper' ) {
-        gameResult.innerHTML = playerWin;
-        return 'playerWin';
-      } else {
-        gameResult.innerHTML = computerWin;
-        return 'computerWin';
-      }
+   }
+ }
+// to update score
+const score = () => {
+  const pScore = document.querySelector('.player-score p');
+  const cScore = document.querySelector('.computer-score p');
+  pScore.textContent = playerScore;
+  cScore.textContent = computerScore;
+}
+
+const computerPlay = () => {
+  const pick = ['rock', 'paper', 'scissors'];
+    return pick[Math.floor(Math.random() * pick.length)];
+}
+const playRound = (playerSelection, computerSelection) => {
+  if(playerSelection === computerSelection) {
+      gameResult.textContent = 'Draw!';
+      return;
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+      gameResult.textContent = 'You win this round!';
+      playerScore++;
+      score();
+      return;
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+      gameResult.textContent = 'You win this round!';
+      playerScore++;
+      score();
+      return;
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper' ) {
+      gameResult.textContent = 'You win this round!';
+      playerScore++;
+      score();
+      return;
+    } else {
+      gameResult.textContent = 'You lose this round';
+      computerScore++;
+      score();
+      return;
     }
-    
-    function score() {
-      scoreLeft.innerHTML = `${playerScore}`;
-      scoreRight.innerHTML = `${computerScore}`;
-    }
-    
-    const restartBtn = document.createElement('button');
-    restartBtn.className = 'restart';
-    restartBtn.appendChild(document.createTextNode('Play Again'));
-    
-    function gameEnd() {
-      if(playerScore === 5 || computerScore === 5) {
-        document.querySelector('.rock').style.display = 'none';
-        document.querySelector('.paper').style.display = 'none';
-        document.querySelector('.scissors').style.display = 'none';
-    
-        document.querySelector('.buttons').appendChild(restartBtn);
-        restartBtn.addEventListener('click', () => {
-          window.location.reload();
-        })
-        if(playerScore > computerScore) {
-          gameResult.innerHTML = '<b>Yey! You win!</b>';
-        } else if (computerScore > playerScore) {
-          gameResult.innerHTML = '<b>Aww you lose against the computer</b>';
-        }
-      }
-    }
+  }
+
+  //call inner functions
+startGame();
+playMatch();
+gameEnd();
+}
+
+game();
